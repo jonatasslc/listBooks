9fcbaac0bddc71cb1820572192766270f56cd70e
JONATAS SOLCIA MACHADO <jonatas.smachado1@senacsp.edu.br>
2023-08-14 19:37:06 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -20,27 +20,46 @@ const ListContainer = styled.div`
 
 function App() {
   const[books, setBooks] = useState([]);
+  const[search, setSearch] = useState('');
 
   useEffect( () => {
     const url = '/books';
-      api.get(url)
-        .then( (response) => {
+
+    const params = {};
+    if (search) {
+      params.title_like = search
+
+
+      api.get('/books?_embed=books', {params})
+      .then( (response) => {
+        // console.log(response.data)
+        setBooks(response.data)
+      })
+      // console.log('Digitei');
+
+    } else  {
+      // console.log('Não digitei');
+
+      // axios.get('http://localhost:3000/books')
+      api.get(url) 
+      .then( (response) => {
           // console.log(response.data)
           setBooks(response.data)
         })
-  },[])  
+    }
+  },[search])  
 
 
   return (
     <Container>
         <h1>Minhas Lista de Livros</h1>
+        <input type="search" placeholder='Buscar livros - Digite aqui o título do livro' value={search} onChange={(ev) => setSearch(ev.target.value)}/>
         <ListContainer>
           {
             books.map(book => {
               return ( <ListBooks 
                 key={book.url} 
                 books={book}
-                
                   /> )
             })
           }
